422881ea4e0b4c20b35d30124b32e1e5e44ebfb7
Martin Friedrich <martinfriedrich@improbable.io>
2021-02-08 16:49:47 +0100

    add postgres example, working

--- internal/db/db.go
+++ internal/db/db.go
@@ -10,7 +10,7 @@ var DB *gorm.DB
 
 func init() {
 	logger.Log.Debug("init database")
-    dsn := "host=db user=gorm password=example dbname=exmaple port=5132 sslmode=disable TimeZone=UTC"
+    dsn := "host=db user=example password=example dbname=example port=5432 sslmode=disable TimeZone=UTC"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database: " + err.Error())
