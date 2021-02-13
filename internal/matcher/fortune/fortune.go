41be292c2bf36196584b82c114373ad1cc31a317
Neo Vg <neo@gothnet.eu>
2021-02-13 13:43:41 +0100

    ✨ Better parsing of /fortune command

--- internal/matcher/fortune/fortune.go
+++ internal/matcher/fortune/fortune.go
@@ -16,7 +16,7 @@ type Matcher struct {
 }
 
 type helpItem struct {
-	command string
+	command     string
 	description string
 }
 
@@ -40,16 +40,17 @@ func (m Matcher) ProcessRequestMessage(requestMessage telegram.RequestMessage) e
 		return m.sendRandomFortune(requestMessage)
 	}
 
-	// help
+	// Show help
 	if option == "help" {
 		return m.sendHelpResponse(requestMessage)
 	}
 
-	// list
+	// Show list of fortune files
 	if option == "list" {
 		return m.sendListResponse(requestMessage)
 	}
 
+	// Show a random fortune from a specific file
 	if fortune.Exists(option) {
 		return m.sendFortune(requestMessage, option)
 	}
@@ -58,23 +59,26 @@ func (m Matcher) ProcessRequestMessage(requestMessage telegram.RequestMessage) e
 	return fmt.Errorf("could not find cookie file `%s`", option)
 }
 
-// Check if a text starts with /jn or /yn
+// Check if a text starts with /fortune
 func (m Matcher) doesMatch(text string) bool {
 	match, _ := regexp.MatchString(`^/fortune(\s|$)`, text)
 
 	return match
 }
 
-// Check if a text starts with /jn and return the text behind
+// Check if a text starts with /fortune and return the text behind
 func (m Matcher) getOption(text string) string {
-	match, _ := regexp.MatchString(`^/fortune .+`, text)
-	if !match {
-		return ""
-	}
+	// Initialize the regular expression
+	r := regexp.MustCompile(`\s\S+`)
 
-	return text[9:]
+	// Find the first word
+	option := r.FindString(text)
+
+	// Return a trimmed version
+	return strings.TrimSpace(option)
 }
 
+// Send a random fortune from a random file
 func (m Matcher) sendRandomFortune(requestMessage telegram.RequestMessage) error {
 	f, err := fortune.GetRandomFortune()
 	if err != nil {
@@ -84,6 +88,7 @@ func (m Matcher) sendRandomFortune(requestMessage telegram.RequestMessage) error
 	return m.sendFortuneResponse(requestMessage, m.formatFortune(f))
 }
 
+// Send a random fortune from a specific file
 func (m Matcher) sendFortune(requestMessage telegram.RequestMessage, file string) error {
 	f, err := fortune.GetFortune(file)
 	if err != nil {
@@ -93,6 +98,7 @@ func (m Matcher) sendFortune(requestMessage telegram.RequestMessage, file string
 	return m.sendFortuneResponse(requestMessage, m.formatFortune(f))
 }
 
+// Send the formatted text of a fortune cookie
 func (m Matcher) sendFortuneResponse(requestMessage telegram.RequestMessage, text string) error {
 	responseText := text
 
@@ -105,6 +111,7 @@ func (m Matcher) sendFortuneResponse(requestMessage telegram.RequestMessage, tex
 	return telegram.SendMessage(requestMessage, responseMessage)
 }
 
+// Send a list of available /fortune commands
 func (m Matcher) sendHelpResponse(requestMessage telegram.RequestMessage) error {
 	helpItems := make([]helpItem, 0, 0)
 	helpItems = append(helpItems, helpItem{
@@ -139,6 +146,7 @@ func (m Matcher) sendHelpResponse(requestMessage telegram.RequestMessage) error 
 	return telegram.SendMessage(requestMessage, responseMessage)
 }
 
+// Send a list of available fortune files
 func (m Matcher) sendListResponse(requestMessage telegram.RequestMessage) error {
 	files := fortune.GetList()
 
@@ -160,6 +168,7 @@ func (m Matcher) sendListResponse(requestMessage telegram.RequestMessage) error 
 	return telegram.SendMessage(requestMessage, responseMessage)
 }
 
+// Try to format a fortune cookie in a way which is nice to read in Telegram
 func (m Matcher) formatFortune(text string) string {
 	lines := strings.Split(text, "\n")
 
@@ -170,31 +179,45 @@ func (m Matcher) formatFortune(text string) string {
 	return text
 }
 
+// Check if a bunch of lines look like a quote (last line begins with `--`)
 func (m Matcher) looksLikeQuote(lines []string) bool {
 	isQuote, _ := regexp.MatchString(`^\s*-- `, lines[len(lines)-1])
 
 	return isQuote
 }
 
+// Format a bunch of lines as quote
+// Tries to remove hard wraps while preserving intentional line breaks
+// The quote itself will become italic, the last line with the author info will remain normal text
 func (m Matcher) formatQuoteFortune(lines []string) string {
 	text := ""
 	for i, line := range lines {
+		// Ignore the last line (with the author of the quote)
 		if i < len(lines) - 1 {
+			// Every line in the formatted quote starts with <i>
 			if len(text) == 0 || text[len(text)-1:] == "\n" {
 				text = text + "<i>"
 			}
+			// Append the next line to the new text
 			text = text + line
+			// We view line breaks at the end of lines of 50 characters or more as  hard wraps (like here:)),
+			// while we view lines with less than 50 characters as end of a paragraph
 			if len(line) >= 50 {
+				// Long lines will be directly followed by the next line, the line break is removed
 				if i == len(lines) - 2 {
+					// The last line of a quote always ends with </i> and a line break, regardless of its length
 					text = text + "</i>\n"
 				} else {
+					// Normally long line just get the next line appended after a white space
 					text = text + " "
 				}
 			} else {
+				// Short lines probably end a paragraph, hence they are followed by </i> and a line break
 				text = text + "</i>\n"
 			}
 		}
 	}
 
+	// Return the quote followed by the unformatted author info
 	return text + "\n" + lines[len(lines)-1]
 }
