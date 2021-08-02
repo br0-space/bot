cb92216617240f819a9bd464420ce28645067c28
Neo Vg <neo@gothnet.eu>
2021-08-02 20:53:41 +0200

    ✨ Slightly nicer output

--- internal/matcher/music/music.go
+++ internal/matcher/music/music.go
@@ -82,29 +82,28 @@ func (m Matcher) processMatch(requestMessage telegram.RequestMessage, match stri
 
 func (m Matcher) sendResponse(requestMessage telegram.RequestMessage, songlinkEntry SonglinkEntry) error {
 	responseText := fmt.Sprintf(
-		"%s:\n\n%s\n(%s)\n\n",
-		songlinkEntry.Type,
+		"<b>%s</b> by <b>%s</b>\n\n",
 		songlinkEntry.Title,
 		songlinkEntry.Artist,
 	)
 
 	// The entry may not be available at each platform, so just add existing links
 	if songlinkEntry.SpotifyURL != "" {
-		responseText += fmt.Sprintf("Spotify: %s\n\n", songlinkEntry.SpotifyURL)
+		responseText += fmt.Sprintf("· Spotify: %s\n\n", songlinkEntry.SpotifyURL)
 	}
 	if songlinkEntry.AppleMusicURL != "" {
-		responseText += fmt.Sprintf("Apple Music: %s\n\n", songlinkEntry.AppleMusicURL)
+		responseText += fmt.Sprintf("· Apple Music: %s\n\n", songlinkEntry.AppleMusicURL)
 	}
 	if songlinkEntry.YoutubeURL != "" {
-		responseText += fmt.Sprintf("YouTube: %s\n\n", songlinkEntry.YoutubeURL)
+		responseText += fmt.Sprintf("· YouTube: %s\n\n", songlinkEntry.YoutubeURL)
 	}
 
 	// Add link so Songlink page for convenience
-	responseText += fmt.Sprintf("Weitere Links bei Songlink: %s", songlinkEntry.SonglinkURL)
+	responseText += fmt.Sprintf("Weitere Links: %s", songlinkEntry.SonglinkURL)
 
 	responseMessage := telegram.Message{
 		Text: responseText,
-		// ParseMode: "Markdown",
+		ParseMode: "HTML",
 		DisableWebPagePreview: true,
 	}
 
